2c18f57ac518ded52114da33fd00c41c06699541
OLayemii <donyemisco@gmail.com>
2018-12-13 03:15:26 +0100

    fixes #1

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,6 +9,14 @@ class LoginController extends Controller
 {
     //
     public function login(Request $request){
-        return response($request->all(), Response::HTTP_OK);
+        $formdata = $request->formdata;
+        if(Auth::attempt(['email' => $formdata['email'], 'password' => $formdata['password']])){
+            $user = Auth::user();
+            $success['token'] =  $user->createToken('MyApp')->accessToken;
+            return response()->json(['success' => $success], Response::HTTP_OK);
+        }
+        else{
+            return response()->json(['error'=>'Unauthorised'], 401);
+        }
     }
 }
